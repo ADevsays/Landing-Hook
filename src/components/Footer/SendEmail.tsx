bef83bf364fa88d0afd50301e00b671d796a3cf8
MauWebs <mauriciovidalwebs@gmail.com>
2023-11-26 17:25:25 -0300

    form-hotmail-h-full

--- src/components/Footer/SendEmail.tsx
+++ src/components/Footer/SendEmail.tsx
@@ -5,7 +5,7 @@ function SendEmail() {
                 <label className="h-[40px] flex relative">
                     <input className="h-full w-full rounded-2xl outline-none ps-5 text-primary" type="email" name="email" placeholder="Email" required/>
                     <input type="hidden" name="_captcha" value="false"></input>
-                    <button type="submit" className="absolute right-0 rounded-none rounded-r-2xl px-6 btn-primary">Enviar</button>
+                    <button type="submit" className="absolute right-0 rounded-none rounded-r-2xl px-6 btn-primary h-full">Enviar</button>
                 </label>
             </form>
         </>
