0f57db991c6fbd7ce40aa94993a26737cd983380
RafaelCTobar <rafaelctobar569@gmail.com>
2023-11-26 14:14:40 -0500

    fixed bug of blob img

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -3,7 +3,7 @@ import LogoHook from '../../Imgs/Logo HOOK.webp';
 function Logo() {
     return (
         <a href="/">
-            <span className='flex-center w-100 h-100 gap-0'>
+            <span className='flex-center w-100 h-100 gap-0 min-w-[100px]'>
                 <img className='h-[55px] w-100 m-0' src={LogoHook} alt="Logo de la empresa de desarrollo de Software Hook"/>
                 <span className='font-bold mt-2 text-lg'>HOOK</span>
             </span>
