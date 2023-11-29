0fab2c8e0795a33b11aba500a80c34f198f8e81e
RafaelCTobar <rafaelctobar569@gmail.com>
2023-11-29 17:02:01 -0500

    add pdf afiliade"

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,8 @@
-import Button from "../Button";
 import SocialMediaMap from "../SocialMediaMap";
 import FooterItem from "./FooterItem";
 import SendEmail from "./SendEmail";
+import AfiliadePDF from '../../pdf/Hook_-_Afiliados.pdf';
+import Button from "../Button";
 
 function Footer() {
     return (
@@ -11,9 +12,9 @@ function Footer() {
                 content="Contamos con redes sociales
                 en las que puedes comunicarte con nosotros"
                 className="border-b lg:border-r border-gray-700 lg:border-b-0 pb-20">
-                    <span className="mt-4 block absolute left-[30px]">
-                        <SocialMediaMap/>
-                    </span>
+                <span className="mt-4 block absolute left-[30px]">
+                    <SocialMediaMap />
+                </span>
             </FooterItem>
 
             <FooterItem
@@ -21,16 +22,19 @@ function Footer() {
                 content="Contamos con un plan para afiliados
                 si estas interesado en unirte y ganar comisiones de hasta el 30%"
                 className="border-b lg:border-r border-gray-700 lg:border-b-0">
-                    <Button className="mt-3 btn-primary">
+                    <Button
+                        className="btn-primary mt-4 relative top-[20px]"
+                        target="_blank"
+                        href={AfiliadePDF}>
                         Conviértete en afiliado
                     </Button>
             </FooterItem>
             <FooterItem
                 title="Contáctanos"
                 content="Envíanos tu correo para contactarte">
-                    <SendEmail/>
+                <SendEmail />
             </FooterItem>
-            
+
         </footer>
     );
 }
